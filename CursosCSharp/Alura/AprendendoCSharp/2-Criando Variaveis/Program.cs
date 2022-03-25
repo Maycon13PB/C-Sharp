bbb2487bba4dd8c44352b7de5dc29f31c78f6fab
Maycon <86448117+Maycon13PB@users.noreply.github.com>
2022-03-25 20:34:20 -0300

    Atualização do C#

---
+++ CursosCSharp/Alura/AprendendoCSharp/2-Criando Variaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Criando_Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exxecutando projeto 2 - Criando Variaveis");
+
+            int idade;
+            idade = 32;
+
+            Console.WriteLine(idade);
+
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
