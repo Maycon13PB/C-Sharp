559b985774a0c90abc0ff5e878707071d4c993b0
Maycon <86448117+Maycon13PB@users.noreply.github.com>
2022-08-04 20:56:06 -0300

    Atualização dos arquivos

---
+++ CursosCSharp/Alura/AprendendoCSharp/ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Cliente gabriela = new Cliente();
+
+            //gabriela.nome = "Gabriela";
+            //gabriela.profissao = "Desenvolvedora C#";
+            //gabriela.cpf = "434.562.878";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            //conta.titular = gabriela;
+            conta.titular = new Cliente();
+            conta.titular.nome = "Gabriela Costa";
+            conta.titular.cpf = "434.562.878" ;
+            conta.titular.profissao = " Desenvolvedora c#";
+
+            conta.saldo = 500;
+            conta.agencia = 563;
+            conta.numero = 5634527;
+
+            //Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CursosCSharp/Alura/AprendendoCSharp/ByteBank/07-ByteBank/ContaCorrente.cs
@@ -0,0 +1,87 @@
+﻿
+namespace _07_ByteBank
+{
+    public class ContaCorrente
+    {
+        public static int TotalDeContasCriadas { get; private set; }
+        private int _agencia;
+
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    return;
+                }
+
+                _agencia = value;
+            }
+        }
+        public int Numero;
+        private double _saldo = 100;
+        public Cliente Titular { get; set; }
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value <0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                return true;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                _saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+
+    }
+}
+
